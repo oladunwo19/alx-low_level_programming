dd7e707f5ced3ab785da5f41e2ab77ea5f78f468
oladunwo aminat <oladunwo19@gmail.com>
2023-08-10 19:44:03 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
-#include<stdio.h> 
+#include <stdio.h> 
 /**
- * main-this is the main function
+ * main- entry point
  *
- * return: always 0
+ * This is the main function
+ *
+ * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
