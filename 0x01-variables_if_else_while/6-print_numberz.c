c637dc8c9ceec113d8c9cac0442393abfb4f025a
oladunwo aminat <oladunwo19@gmail.com>
2023-08-14 10:53:52 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int Q;
+
+	for (Q = 0; Q < 0; Q++)
+	{
+		putchar(Q + '0');
+	}
+putchar('\n');
+return (0);
+}
