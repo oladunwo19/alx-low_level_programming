9d27367ea4d947512db5cfb5ace51135ebb13a5e
oladunwo aminat <oladunwo19@gmail.com>
2023-08-14 11:05:58 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char Q;
+
+	for (Q = 'z'; Q <= 'a'; Q += 1)
+	{
+		putchar(Q);
+	}
+putchar ('\n');
+return (0);
+}
