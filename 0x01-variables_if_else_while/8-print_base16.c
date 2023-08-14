c8470cb112ded1994e9fd0f99f397a90e0214aff
oladunwo aminat <oladunwo19@gmail.com>
2023-08-14 11:44:33 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,10 +9,10 @@ int main(void)
 	int i;
 	char j;
 
-	for (i = 0; i < 'g'; i++)
+	for (i = 0; i < 10; i++)
 		putchar(i + '0');
 	for (j = 'a'; j < 'g'; j++)
-		putchar(j + '0');
+		putchar(j);
 putchar('\n');
 return (0);
 }
