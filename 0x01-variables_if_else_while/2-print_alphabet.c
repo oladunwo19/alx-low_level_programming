1f83c1077d89f3ee73878e5a6e0acb9f16779820
oladunwo aminat <oladunwo19@gmail.com>
2023-08-14 04:25:14 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char B;
+
+	for (B = 'a'; B <= 'z'; B += 1)
+	{
+		putchar(B);
+	}
+putchar('\n');
+return (0);
+}
