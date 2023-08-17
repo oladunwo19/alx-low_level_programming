8891efaced7d21db1a3d374b2a1802adde52567e
oladunwo <oladunwo19@gmail.com>
2023-08-16 20:44:22 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - function name
+ * @n: number
+ * Return: nothing
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i < 98; i++)
+		{
+			printf("%d, ", i);
+		}
+		printf("%d", 98);
+	}
+	else if (n == 98)
+	{
+		printf("%d", n);
+	}else
+	{
+		for (i = n; i > 98; i--)
+		{
+			printf("%d, ", i);
+		}
+		printf("%d", 98);
+	}
+	printf("\n");
+}
