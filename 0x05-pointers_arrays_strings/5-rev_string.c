ea60cc18bfb3f6a73de99c9049a57abcc7bf5e2d
oladunwo <oladunwo19@gmail.com>
2023-08-22 19:24:39 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -16,10 +16,10 @@ void rev_string(char *s)
 	}
 	for (i = 0; i < count / 2; i++)
 	{
-	char j;
+		char j;
 
 		j = s[i];
 		s[i] = s[count - 1 - i];
-		s[count - 1 - i] = j;
+		s[count - 1 - i] =  j;
 	}
 }
