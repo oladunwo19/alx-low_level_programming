d729058c577a7427afe10a30471b6e8dd837649e
oladunwo <oladunwo19@gmail.com>
2023-08-22 19:38:00 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of spring
+ * @str: parameter to be printed
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i, n, count = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
+	n = (count - 1) / 2;
+	for (i = n + 1; str[i] != '\0'; i++)
+		putchar(str[i]);
+	putchar('\n');
+}
