bc6807ac35d96cc5322ab440eaa91984221c4b5c
oladunwo <oladunwo19@gmail.com>
2023-08-23 18:53:34 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,9 +15,10 @@ char *_strcat(char *dest, char *src)
 
 	for (i = 0; dest[i] != '\0'; i++)
 		destlen++;
-			for (i = 0; src[i] != '\0'; i++)
-				srclen++;
+	for (i = 0; src[i] != '\0'; i++)
+		srclen++;
+
 	for (i = 0; i < srclen; i++)
-		dest[destlen] = src[i];
+		dest[destlen + i] = src[i];
 	return (dest);
 }
